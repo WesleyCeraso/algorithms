05ae907fa4f7c34ae5c5faf7d09776044bc659cd
Wesley Ceraso Prudencio <wesleyceraso@gmail.com>
2015-11-12 15:46:00 -0200

    Removes unnecessary code from Speed test
    Signed-off-by: Wesley Ceraso Prudencio <wesleyceraso@gmail.com>

--- test/functional/test_speed.cpp
+++ test/functional/test_speed.cpp
@@ -5,30 +5,20 @@
 
 TEST(Speed, DenseDense)
 {
-    size_t tamanho = 1000;
+    size_t tamanho = 200;
     DenseMatrix<double> m1(tamanho, tamanho, 5);
     DenseMatrix<double> m2(tamanho, tamanho, 10);
 
-    clock_t c1 = clock();
-
     DenseMatrix<double> m3 = m1 * m2;
     //std::cout << std::setw(6) << std::setprecision(3) << m3;
-
-    clock_t c2 = clock();
-    std::cout << "Tempo: " << (c2 - c1) / (double) CLOCKS_PER_SEC << std::endl;
 }
 
 TEST(Speed, CompressedCompressed)
 {
-    size_t tamanho = 500;
+    size_t tamanho = 200;
     CompressedMatrix<double> m1(tamanho, tamanho, 5);
     CompressedMatrix<double> m2(tamanho, tamanho, 10);
 
-    clock_t c1 = clock();
-
     CompressedMatrix<double> m3 = m1 * m2;
     //std::cout << std::setw(6) << std::setprecision(3) << m3;
-
-    clock_t c2 = clock();
-    std::cout << "Tempo: " << (c2 - c1) / (double) CLOCKS_PER_SEC << std::endl;
 }
